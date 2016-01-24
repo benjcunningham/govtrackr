dd5f0f6ce968390feb14ceb206c965516e52647b
Ben Cunningham <benjamin-cunningham@uiowa.edu>
2016-01-23 18:28:03 -0600

    Initialize main query function

---
+++ R/fetch_govtrack.R
@@ -0,0 +1,91 @@
+#' Retrieve data from the GovTrack.us API
+#'
+#' Queries a GovTrack.us API resource for a data set using server-side
+#' filtering, sorting, and pagination.
+#' @param res Resource identifier
+#' @param filter Named list of query filters
+#' @param sort Vector of fields on which to sort
+#' @param limit Maximum number of results to return
+#' @param offset Pagination index of results
+#' @return Data frame of results. Note that observations are returned
+#'   according to the structure returned by the server and may not be
+#'   wholly atomic.
+#' @import magrittr
+#' @export
+fetch_govtrack <- function(res = "bill", filter, sort, limit, offset) {
+
+  res_url <- resources$resource_url[which(resources$resource == res)]
+
+  if (length(res_url) == 0) {
+    stop("The resource `", res, "` does not exist at GovTrack.")
+  }
+
+  if(!missing(filter)) {
+
+    bad_filters <- .bad_filters(res, filter)
+    if (sum(bad_filters) > 0) {
+      paste(names(filter)[bad_filters], collapse = "`, `") %>%
+        stop("The following fields cannot be used for filtering: `",
+             ., "`", call. = FALSE)
+    }
+
+  }
+
+  query <- .create_query(res_url, filter, sort, limit, offset)
+  df <- jsonlite::fromJSON(query)$objects
+
+  return(df)
+
+}
+
+#' Identify improper filters
+#'
+#' @inheritParams fetch_govtrack
+#' @rdname fetch_govtrack
+#' @return Named vector the same length as \code{filter} identifying
+#' improper filters with 1 and proper filters with 0.
+.bad_filters <- function(res, filter) {
+
+  sapply(names(filter),
+         function(x) !(x %in% fields$field[fields$resource == res &
+                                           fields$filterable]))
+
+}
+
+#' Create API query string
+#'
+#' @param res_url Resource root URL
+#' @inheritParams fetch_govtrack
+#' @rdname fetch_govtrack
+#' @return Character string representing the constructed API query
+.create_query <- function(res_url, filter, sort, limit, offset) {
+
+  q <- NULL
+
+  if (!missing(filter)) {
+
+    q <-
+      paste0(names(filter), "=", filter) %>%
+      paste(collapse = "&") %>%
+      paste0(res_url, "?", .) %>%
+      paste0(q, .)
+
+  }
+
+  if (!missing(sort)) {
+    q <-
+      paste(sort, collapse = "|") %>%
+      paste0(q, '&sort=', .)
+  }
+
+  if (!missing(limit)) {
+    q <- paste0(q, '&limit=', limit)
+  }
+
+  if (!missing(offset)) {
+    q <- paste0(q, '&offset=', offset)
+  }
+
+  return(q)
+
+}
